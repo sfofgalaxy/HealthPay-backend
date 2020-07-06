3e8098162af80ebae56a541c996b5a379eb27824
sfofgalaxy <3170105860@zju.edu.cn>
2020-07-06 11:28:03 +0800

    更新

--- src/main/java/com/g16/healthpay/controller/UserController.java
+++ src/main/java/com/g16/healthpay/controller/UserController.java
@@ -1,24 +1,23 @@
 package com.g16.healthpay.controller;
 
-import com.g16.healthpay.dto.GeneralMessage;
-import com.g16.healthpay.intercepter.AuthToken;
-import com.g16.healthpay.service.UserServiceImpl;
-import com.g16.healthpay.utils.RedisUtils;
 import com.g16.healthpay.dto.LoginMessage;
+import com.g16.healthpay.service.UserService;
 import com.g16.healthpay.utils.EncrypteUtils;
+import com.g16.healthpay.utils.RedisUtils;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
-import javax.annotation.Resource;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
 //    @Resource
 //    @Autowired
-    @Resource
-    UserServiceImpl userServiceImpl;
+    @Autowired
+    UserService userService;
     @Autowired
     RedisUtils redisUtils;
     @Autowired
