e9820beaa40dbdb81e9471e1027dd0065a0d65fd
sfofgalaxy <3170105860@zju.edu.cn>
2020-07-07 14:22:21 +0800

    验证码服务

--- src/main/java/com/g16/healthpay/service/UserServiceImpl.java
+++ src/main/java/com/g16/healthpay/service/UserServiceImpl.java
@@ -1,9 +1,14 @@
 package com.g16.healthpay.service;
 
+import com.aliyuncs.dysmsapi.model.v20170525.SendSmsResponse;
+import com.aliyuncs.exceptions.ClientException;
 import com.g16.healthpay.mapper.UserDao;
+import com.g16.healthpay.utils.CaptchaUtils;
 import com.g16.healthpay.utils.EncrypteUtils;
 import com.g16.healthpay.utils.RedisUtils;
+import com.g16.healthpay.utils.SmsUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
@@ -16,6 +21,13 @@ public class UserServiceImpl implements UserService {
     EncrypteUtils encrypteUtils;
     @Autowired
     RedisUtils redisUtils;
+    @Autowired
+    CaptchaUtils captchaUtils;
+    @Autowired
+    SmsUtils smsUtils;
+    // 短信模板id
+    @Value("${spring.sms.template-code}")
+    String templateCode;
 
     @Override
     public String verify(String phone,String captcha){
@@ -33,11 +45,16 @@ public class UserServiceImpl implements UserService {
 
 
     @Override
-    public String sendCaptcha(String phone) {
-        String captcha = encrypteUtils.getMD5Code(phone).substring(0,6);
-
-        redisUtils.setCaptcha(phone,captcha);
-        return captcha;
+    public boolean sendCaptcha(String phone) throws ClientException {
+        // 验证码（指定长度的随机数）
+        String captcha = captchaUtils.generateVerifyCode(6);
+        String templateParam = "{\"code\":\""+captcha+"\"}";
+        SendSmsResponse response = smsUtils.sendSms(phone,templateParam,templateCode);
+        if( response.getCode().equals("OK")) {
+            redisUtils.setCaptcha(phone,captcha);
+            return true;
+        }
+        return false;
     }
 
 
