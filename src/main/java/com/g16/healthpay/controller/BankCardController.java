35e02010582fc1b04db41d643c2d8dd8545e4ad9
sfofgalaxy <3170105860@zju.edu.cn>
2020-07-08 23:43:46 +0800

    update

--- src/main/java/com/g16/healthpay/controller/BankCardController.java
+++ src/main/java/com/g16/healthpay/controller/BankCardController.java
@@ -17,8 +17,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class BankCardController {
     @Autowired
     BankCardService bankCardService;
-    @Autowired
-    RedisUtils redisUtils;
 
     @ApiOperation("绑定银行卡")
     @RequestMapping(value = "/bindBankCard",method = RequestMethod.POST)
