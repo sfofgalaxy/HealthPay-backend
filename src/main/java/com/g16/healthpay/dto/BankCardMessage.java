23f2ea85b090b6a6a6444432e7c56db10a9211e2
Mingkang Hui <huimingkang@126.com>
2020-07-08 23:41:23 +0800

    7.7 Updates

---
+++ src/main/java/com/g16/healthpay/dto/BankCardMessage.java
@@ -0,0 +1,13 @@
+package com.g16.healthpay.dto;
+
+
+import com.g16.healthpay.model.BankCard;
+import lombok.Data;
+
+import java.util.List;
+
+
+
+public class BankCardMessage {
+    List<BankCard> bankCards;
+}
