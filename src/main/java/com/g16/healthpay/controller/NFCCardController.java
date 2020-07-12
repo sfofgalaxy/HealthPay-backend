5d0e8571e99addc00f79d922e1e524e0e678ade0
Mingkang Hui <huimingkang@126.com>
2020-07-12 23:01:57 +0800

    7.7 Updates

---
+++ src/main/java/com/g16/healthpay/controller/NFCCardController.java
@@ -0,0 +1,31 @@
+package com.g16.healthpay.controller;
+
+import com.g16.healthpay.dto.BillMessage;
+import com.g16.healthpay.dto.GeneralMessage;
+import com.g16.healthpay.dto.BankCardMessage;
+import com.g16.healthpay.intercepter.AuthToken;
+import com.g16.healthpay.model.BankBill;
+import com.g16.healthpay.model.BankCard;
+import com.g16.healthpay.service.BankCardService;
+import com.g16.healthpay.utils.EncrypteUtils;
+import com.g16.healthpay.utils.RedisUtils;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/NFC")
+public class NFCCardController {
+
+
+    @ApiOperation("存储NFC tag")
+    @RequestMapping(value = "/saveTag",method = RequestMethod.POST)
+    public GeneralMessage login(@RequestHeader("token") String token,
+                                @RequestParam("tag") String tag){
+        GeneralMessage message=null;
+        return message;
+    }
+}
